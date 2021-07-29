e3508aaf764820b3cf12f7c1730ac898220a3499
Fredrik Engstrand <fredrik@engstrand.nu>
2021-07-29 16:47:52 +0200

    fix: use absolute path to dwl patches to prevent warnings

--- dwl-guile/patches.scm
+++ dwl-guile/patches.scm
@@ -7,10 +7,17 @@
                          make-patch
                          list-of-local-files?))
 
+; Find the absolute path to home-dwl-service by looking in the
+; Guile load path.
+(define %patch-directory
+  (find (lambda (path)
+          (file-exists? (string-append path "/patches/dwl-guile.patch")))
+        %load-path))
+
 (define (list-of-local-files? val) (every local-file? val))
 
 (define (make-patch file-name)
-  (local-file (string-append "patches/" file-name ".patch")))
+  (local-file (string-append %patch-directory "/patches/" file-name ".patch")))
 
 ; TODO: use absolute path to patches
 ; If we assume that `home-dwl-service` will be installed as a package,
